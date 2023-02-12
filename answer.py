4f2a6c0ffc5a5b4a2d98d95f689fc7efdd91755d
zhongxiao0502 <wzx20000502@163.com>
2023-02-12 16:38:01 -0500

    second change

--- answer.py
+++ answer.py
@@ -83,7 +83,7 @@ def list_2D():
 
     # Collect the items in the last column of matrix A using list comprehension and assign it to a new variable last_column.
 
-    last_column = A[2]
+    last_column = [row[2] for row in A]
 
     # Get the item at the last row and last column of A.
 
